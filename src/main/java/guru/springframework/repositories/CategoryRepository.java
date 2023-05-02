776f848099d13b88cf080ef01ae15ff33ac9035b
Gaspar Oliveira <goliveira@noxhealth.com>
2023-05-02 15:14:14 +0100

    no message

--- src/main/java/guru/springframework/repositories/CategoryRepository.java
+++ src/main/java/guru/springframework/repositories/CategoryRepository.java
@@ -1,7 +1,6 @@
 package guru.springframework.repositories;
 
 import guru.springframework.domain.Category;
-import guru.springframework.domain.Recipe;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
